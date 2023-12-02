14683fd1234f0dcb477e4c3d4979d84c28b052d9
Tawonga021 <reginaldjongwe@gmail.com>
2023-12-02 10:15:42 -0800

    added cors policy

--- app.js
+++ app.js
@@ -28,16 +28,35 @@ app.listen(process.env.PORT,()=>{
 })
 
  connect(process.env.MONGODB_URI);
- app.use("/api/v1/admin/",adminRoutes);
+ // Add headers
+app.use(function (req, res, next) {
 
- app.use("/api/v1/manager/",managerRoutes);
- app.use("/api/v1/customer/",customerRoutes);
- app.use("/api/v1/till-operator/",tillOperatorRoute);
- app.use("/api/v1/product/",productRouter);
- app.use("/api/v1/till/",tillRouter);
- app.use("/api/v1/sale/",salesRouter);
- app.use("/api/v1/cart/",cartRouter);
- app.use("/api/v1/order/",orderRouter);
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use("/api/v1/admin/",adminRoutes);
+
+app.use("/api/v1/manager/",managerRoutes);
+app.use("/api/v1/customer/",customerRoutes);
+app.use("/api/v1/till-operator/",tillOperatorRoute);
+app.use("/api/v1/product/",productRouter);
+app.use("/api/v1/till/",tillRouter);
+app.use("/api/v1/sale/",salesRouter);
+app.use("/api/v1/cart/",cartRouter);
+app.use("/api/v1/order/",orderRouter);
 app.use("/api/v1/transaction/",transactionRouter);
 app.use("/api/v1/statistics/",aggregateRouter);
 
