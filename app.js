b986719eabc089a0eda0ee5d68910aa4f354763f
Tawonga021 <reginaldjongwe@gmail.com>
2023-12-09 12:51:46 -0800

    added cors options

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ dotenv.config();
 
 
 app.use(express.json());
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 
 app.listen(process.env.PORT,()=>{
     console.log(`app running on port ${process.env.PORT}`);
